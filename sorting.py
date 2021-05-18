84f048359e9c70e85b6f1e619956d2ffae65d81e
Chirag Chadha <chiragchadhairl@gmail.com>
2021-05-18 22:39:18 +0100

    Sorting implementations

---
+++ sorting.py
@@ -0,0 +1,114 @@
+import math
+
+
+# O(n**2). O(1) space. Not stable.
+def selection_sort(seq):
+    """For each index in seq, swap with minimum value from the remaining values."""
+    for idx, elem in enumerate(seq):
+        minimum = math.inf
+        for other_idx, other_elem in enumerate(seq[idx:], start=idx):
+            if minimum > other_elem:
+                minimum = other_elem
+                min_idx = other_idx
+        if minimum != math.inf:
+            # Swap elements.
+            seq[idx], seq[min_idx] = minimum, elem
+    return seq
+
+
+# O(n**2). O(1) space. Stable sort.
+def insertion_sort(seq):
+    """For each element, compare and swap with elements to the left."""
+    for idx, elem in enumerate(seq):
+        other_idx = idx - 1
+        while other_idx >= 0:
+            other_elem = seq[other_idx]
+            if elem > other_elem:
+                break
+
+            # Swap elements.
+            seq[idx], seq[other_idx] = other_elem, elem
+            other_idx -= 1
+
+    return seq
+
+
+# O(n**3/2) or thereabouts. Not necessarily quadratic. Not stable.
+def shell_sort(seq):
+    """Initalize h as a steadily large value, h-sort, and steadily decrease h to completely sort."""
+    h = 1
+     # 1, 4, 13, 40, 121, 364, 1093, ...
+    while h < len(seq) / 3:
+        h = 3*h + 1
+
+    while h >= 1:
+        for idx in range(h, len(seq)):
+            other_idx = idx
+            while other_idx >= h and seq[other_idx] < seq[other_idx - h]:
+                seq[other_idx], seq[other_idx - h] = seq[other_idx - h], seq[other_idx]
+                other_idx -= h
+        h //= 3
+
+    return seq
+
+
+# O(nlogn) guaranteed worst case and O(n) space for auxiliary array.
+def merge_sort(seq):
+    aux = [None] * len(seq)
+
+    def merge(seq, lo, mid, hi):
+        """Merge subarrays"""
+        first_seq_start, second_seq_start = lo, mid + 1
+        aux[lo:hi] = seq[lo:hi]
+
+        for idx in range(lo, hi + 1):
+            if first_seq_start > mid:
+                seq[idx] = aux[second_seq_start]
+                second_seq_start += 1
+            elif second_seq_start > hi:
+                seq[idx] = first_seq_start
+                first_seq_start += 1
+            elif aux[second_seq_start] < aux[first_seq_start]:
+               seq[idx] = second_seq_start
+               second_seq_start += 1
+            else:
+                seq[idx] = aux[first_seq_start]
+                first_seq_start += 1
+
+    def sort(seq, lo, hi):
+        """Recursively split seq into two subarrays and sort (stable)."""
+        if hi <= lo:
+            return
+        #mid = lo + (hi - lo) // 2
+        mid = (hi + lo) // 2
+        print(lo, mid, hi)
+        sort(seq, lo=lo, hi=mid)
+        sort(seq, lo=mid + 1, hi=hi)
+        merge(seq, lo, mid, hi)
+
+    sort(seq, lo=0, hi=len(seq))
+    return seq
+
+# O(nlogn) average case. Generally faster than mergesort due to less data movement. O(n**2) at worst.
+# Between O(logn) and O(n) space complexity due to recursive sort calls.
+def quick_sort(seq):
+    """Scan seq from both left and right (converging in center) and sort by exchanging values."""
+
+
+# O(n) best case with equal keys. O(n**2) worst case and O(nlogn) average case (same as quicksort).
+# Uses O(logn) space for recursion.
+def three_way_quick_sort(seq):
+    """Quicksort variation used to quickly sort sequences with duplicate elements"""
+
+if __name__ == "__main__":
+    arr = [4, 3, 1, 5, 6]
+
+    print(selection_sort(arr))
+    print(insertion_sort(arr))
+    print(shell_sort(arr))
+    print(merge_sort(arr))
+
+    arr = [8, 2, 6, 3, 1, 5, 7, 8, 44, 1, 3, 344, 5, 7, 2, 3, 9, 1, 2, 3, 4]
+    print(selection_sort(arr))
+    print(insertion_sort(arr))
+    print(shell_sort(arr))
